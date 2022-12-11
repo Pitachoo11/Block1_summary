d312a013663f2ebf5d55ca6ec3d0943c1b887a6e
Petr.Churkin <Petr.Churkin@gmail.com>
2022-12-11 21:26:18 +0300

    Program.cs added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿ 
+ void FindUnder3Digits(string[] Array)
+{
+    var TargetArray = new string[Array.Length];
+    var TargetArraySize = 0;
+        foreach (var value in Array )
+        {
+            if (value.Length <= 3)
+            {
+                TargetArray[TargetArraySize] = value;
+                TargetArraySize++;
+            }
+        }
+    Console.WriteLine($"[ {String.Join(", ", TargetArray, 0, TargetArraySize)} ]");
+    Console.WriteLine();    
+}           
+
+Console.WriteLine("Введите произвольное число слов/чисел через пробел:");
+string input = Console.ReadLine();
+string[] InputArray = input.Split(' ');
+
+FindUnder3Digits(InputArray);
